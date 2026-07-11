c088b49e47afb33a33a0bd34d3a976e4aa416586
kpunam62031-coder <kpunam62031@gmail.com>
2026-07-11 14:44:52 +0530

    Create MainActivity.java

---
+++ app/app/src/main/app/src/main/java/com/example/futuregram/app/src/main/java/com/example/futuregram/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.futuregram;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        webView = new WebView(this);
+        setContentView(webView);
+
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+
+        webView.loadUrl("file:///android_asset/index.html");
+    }
+}
